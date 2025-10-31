49049c0644179250ba1dfe67fd301d5173d5c26a
Lee <mklmkcmkt@outlook.com>
2025-10-31 07:16:18 +0000

    tainted the vm_instance and added a provisioner to it

--- main.tf
+++ main.tf
@@ -22,6 +22,11 @@ resource "google_compute_instance" "vm_instance" {
   name         = "terraform-instance"
   machine_type = "e2-micro"
   tags         = ["web", "dev"]
+   
+ provisioner "local-exec" {
+    command = "echo ${google_compute_instance.vm_instance.name}:  ${google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip} >> ip_address.txt"
+  }
+
 
   boot_disk {
     initialize_params {
