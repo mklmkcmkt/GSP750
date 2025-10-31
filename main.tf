39f0fe7fa0d772b053281b6faff8dd9b0add23e4
Lee <mklmkcmkt@outlook.com>
2025-10-31 03:45:04 +0000

    VPC , VM and Network Interface

---
+++ main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+
+  project = "qwiklabs-gcp-01-ed343bfccad5"
+  region  = "us-central1"
+  zone    = "us-central1-f"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
