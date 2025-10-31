3363eb771ba11ba4e762422d5bbe86f1ff094ca1
Lee <mklmkcmkt@outlook.com>
2025-10-31 07:05:55 +0000

    New VM and storage bucket with implicit and explicit dependencies

--- main.tf
+++ main.tf
@@ -29,8 +29,46 @@ resource "google_compute_instance" "vm_instance" {
     }
   }
 
+ network_interface {
+    network = google_compute_network.vpc_network.self_link
+    access_config {
+      nat_ip = google_compute_address.vm_static_ip.address
+    }
+  }
+}
+
+resource "google_compute_address" "vm_static_ip" {
+  name = "terraform-static-ip"
+}
+
+# New resource for the storage bucket the  application will use.
+resource "google_storage_bucket" "example_bucket" {
+  name     = "qwiklabs-gcp-03-fd94a0391124"
+  location = "US"
+
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+}
+
+# New instance that uses the bucket
+resource "google_compute_instance" "another_instance" {
+  # Tells Terraform that this VM instance must be created only after the
+  # storage bucket has been created.
+  depends_on = [google_storage_bucket.example_bucket]
+
+  name         = "terraform-instance-2"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "cos-cloud/cos-stable"
+    }
+  }
+
   network_interface {
-    network = google_compute_network.vpc_network.name
+    network = google_compute_network.vpc_network.self_link
     access_config {
     }
   }
